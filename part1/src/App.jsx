bd953499b2c422271884696d4b50b31c141de5b1
jabir254 <jabirkullow@gmail.com>
2024-03-02 21:39:54 +0300

    more complex state

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,43 +1,48 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+// import Header from "./components/Header";
+// import Content from "./components/Content";
+// import Total from "./components/Total";
 import { useState } from "react";
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>so you are probably born in {bornYear()}</p>
-    </div>
-  );
-};
+// const Hello = ({ name, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age;
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>so you are probably born in {bornYear()}</p>
+//     </div>
+//   );
+// };
 
-const Display = ({ counter }) => <div>{counter}</div>;
+// const Display = ({ counter }) => <div>{counter}</div>;
 
-const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
-};
+// const Button = (props) => {
+//   return <button onClick={props.onClick}>{props.text}</button>;
+// };
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
-
+  const [clicks, setClicks] = useState({ left: 0, right: 0 });
+  const handleLeftClick = () => {
+    const newClicks = {
+      ...clicks,
+      left: clicks.left + 1,
+    };
+    setClicks(newClicks);
+  };
+  const handleRightClick = () => {
+    const newClicks = {
+      ...clicks,
+      right: clicks.right + 1,
+    };
+    setClicks(newClicks);
+  };
   return (
     <div>
-      <div>
-        <Display counter={counter} />
-        <Button onClick={increaseByOne} text="plus" />
-        <Button onClick={setToZero} text="zero" />
-        <Button onClick={decreaseByOne} text="minus" />
-      </div>
-      <Header />
-      <Content />
-      <Total />
+      {clicks.left}
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   );
 };
